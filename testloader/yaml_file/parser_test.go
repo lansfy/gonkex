21c5a156ca27592735fbf1ff03e3dcd63d9d36e3
lansfy <5764541+lansfy@users.noreply.github.com>
2024-12-24 11:55:46 -0700

    new: simplify parser test

--- testloader/yaml_file/parser_test.go
+++ testloader/yaml_file/parser_test.go
@@ -1,71 +1,11 @@
 package yaml_file
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
-var testsYAMLData = `
-- method: POST
-  path: /jsonrpc/v2/orders.nr
-  request:
-    '{
-      "jsonrpc": "2.0",
-      "id": "550e8400-e29b-41d4-a716-446655440000",
-      "method": "orders.nr",
-      "params": [
-        {
-          "amount": 1,
-          "prefix": "ru"
-        }
-      ]
-    }'
-  response:
-    200:
-      '{
-         "result": [
-           {
-             "nr": "number",
-             "prefix": "ru",
-             "vc": "vc"
-           }
-         ],
-         "id": "550e8400-e29b-41d4-a716-446655440000",
-         "jsonrpc": "2.0"
-       }'
-  cases:
-    - requestArgs:
-        foo: 'Hello world'
-        bar: 42
-      responseArgs:
-        200:
-          foo: 'Hello world'
-          bar: 42
-    - requestArgs:
-        foo: 'Hello world'
-        bar: 42
-      responseArgs:
-        200:
-          foo: 'Hello world'
-          bar: 42
-      variables:
-        newVar: some_value
-`
-
 func TestParseTestsWithCases(t *testing.T) {
-	tmpfile, err := ioutil.TempFile("../..", "tmpfile_")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name())
-
-	if _, err := fmt.Fprint(tmpfile, testsYAMLData); err != nil {
-		t.Fatal(err)
-	}
-
-	tests, err := parseTestDefinitionFile(tmpfile.Name())
+	tests, err := parseTestDefinitionFile("testdata/parser.yaml")
 	if err != nil {
 		t.Error(err)
 	}
