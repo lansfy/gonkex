f5ab855d31e12ac2f72ae71ca64fe0a493e7cc2e
lansfy <5764541+lansfy@users.noreply.github.com>
2025-01-08 19:45:04 -0700

    new: move allure functions to allure code

--- models/result.go
+++ models/result.go
@@ -1,7 +1,5 @@
 package models
 
-import "errors"
-
 type DatabaseResult struct {
 	Query    string
 	Response []string
@@ -20,52 +18,6 @@ type Result struct {
 	Errors              []error
 	Test                TestInterface
 	DatabaseResult      []DatabaseResult
-}
-
-func allureStatus(status string) bool {
-	switch status {
-	case "passed", "failed", "broken", "skipped":
-		return true
-	default:
-		return false
-	}
-}
-
-func notRunnedStatus(status string) bool {
-	switch status {
-	case "broken", "skipped":
-		return true
-	default:
-		return false
-	}
-}
-
-func (r *Result) AllureStatus() (string, error) {
-	testStatus := r.Test.GetStatus()
-	if testStatus != "" && allureStatus(testStatus) && notRunnedStatus(testStatus) {
-		return testStatus, nil
-	}
-
-	var (
-		status     = "passed"
-		testErrors []error
-	)
-
-	if len(r.Errors) != 0 {
-		status = "failed"
-		testErrors = r.Errors
-	}
-
-	if len(testErrors) != 0 {
-		errText := ""
-		for _, err := range testErrors {
-			errText = errText + err.Error() + "\n"
-		}
-
-		return status, errors.New(errText)
-	}
-
-	return status, nil
 }
 
 // Passed returns true if test passed (false otherwise)
